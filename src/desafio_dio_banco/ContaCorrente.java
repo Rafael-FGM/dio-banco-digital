0429a8968247c65fdfe0753e58003237c71eb63d
Rafael Ferreira <rafamota2@hotmail.com>
2024-01-24 15:30:26 -0300

    add repositório no git

---
+++ src/desafio_dio_banco/ContaCorrente.java
@@ -0,0 +1,23 @@
+package desafio_dio_banco;
+
+public  class ContaCorrente extends Conta {
+	
+	public ContaCorrente(Cliente cliente) {
+		super(cliente);
+	}
+
+	@Override
+	public void imprimirExtrato() {
+		System.out.println("=== Extrato Conta Corrente === ");
+		super.imprimirInfosComuns();
+	}
+
+	@Override
+	public void imprimirCliente() {
+		System.out.println(cliente);
+		
+		
+	}
+
+	
+}
---
+++ src/desafio_dio_banco/Main.java
@@ -0,0 +1,21 @@
+package desafio_dio_banco;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		Cliente rafael = new Cliente();
+		rafael.setNome("Rafael");
+		
+		Conta cc = new ContaCorrente(rafael);
+		Conta poupanca = new ContaPoupanca(rafael);
+		
+		cc.depositar(100);
+		cc.transferir(100, poupanca);
+		
+		cc.imprimirExtrato();
+		poupanca.imprimirExtrato();
+		
+		cc.imprimirCliente();
+		poupanca.imprimirCliente();
+	}
+}
